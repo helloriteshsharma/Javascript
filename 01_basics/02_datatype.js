b0835d76f8b7a3b23bb1b1e10fc08deefffd6d1a
helloriteshsharma <112851636+helloriteshsharma@users.noreply.github.com>
2024-02-27 10:56:42 +0000

    done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS cod as newer version
+
+//alert(3+3) // 
+
+let name = "Ritesh";
+let age = 18;
+let isLoggedIn = false;
+let state ;
+let city = null ;
+
+
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object
+
+console.log(typeof "Ritesh");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
