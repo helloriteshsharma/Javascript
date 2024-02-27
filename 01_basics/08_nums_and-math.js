42d8e43397cff532ac9f5f259b88dcd2ccc5283f
helloriteshsharma <112851636+helloriteshsharma@users.noreply.github.com>
2024-02-27 13:28:15 +0000

    done

---
+++ 01_basics/08_nums_and-math.js
@@ -0,0 +1,33 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const otherNumber = 123.434;
+console.log(otherNumber.toPrecision(3))
+
+const  hundred =  1000000;
+console.log(hundred.toLocaleString('en-IN'));
+
+
+//******************************************** Maths ***********************************************/
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min(4,3,4,5,6,4,123,5));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20 ;
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
+
